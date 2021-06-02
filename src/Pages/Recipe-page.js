fa8d6fd3b38279a9de55b3ba4f00a6e4324402c8
School <School@Levis-oude-bakbeest.local>
2021-06-02 12:45:26 +0200

    added new components, started working on comment component, WIP

--- src/Pages/Recipe-page.js
+++ src/Pages/Recipe-page.js
@@ -1,8 +1,15 @@
+import Comment from "../components/Comment/Comment";
+import RecipeDetail from "../components/Recipe-detail/Recipe-detail";
+
 function RecipePage() {
     return (
         <div className="page-wrapper">
             <div className="page-wrapper-inner">
                 <h1>Recipe's</h1>
+                <Comment />
+
+                <RecipeDetail />
+
             </div>
         </div>
     )
