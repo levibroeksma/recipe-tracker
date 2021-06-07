5344f7608ccdb91ee56da47749a4cf8397f46ea4
School <School@Levis-oude-bakbeest.local>
2021-06-07 21:07:52 +0200

    fixed minor styling

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -6,21 +6,21 @@ function Input({labelId, name, type, placeholder, errors, register, required,  m
 
     return (
         <label htmlFor={name} id={labelId}>
-                <input
-                    type={type}
-                    className="textInput"
-                    name={name}
-                    id={name}
-                    placeholder={placeholder}
-                    {...register(
-                        name,
-                        {
-                            required: required,
-                            minLength: minLength,
-                            maxLength: maxLength,
-                            pattern: pattern
-                        })}
-                />
+            <input
+                type={type}
+                className="textInput"
+                name={name}
+                id={name}
+                placeholder={placeholder}
+                {...register(
+                    name,
+                    {
+                        required: required,
+                        minLength: minLength,
+                        maxLength: maxLength,
+                        pattern: pattern
+                    })}
+            />
             {errors[name] && errors[name].type === "required" && <span className="errorMessage">{requiredError}</span>}
             {errors[name] && errors[name].type === "minLength" && <span className="errorMessage">{minLengthError}</span>}
             {errors[name] && errors[name].type === "maxLength" && <span className="errorMessage">{maxLengthError}</span>}
