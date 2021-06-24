c62a22cb2056c13e2437542911d68e84ba5b3e4d
School <School@Levis-oude-bakbeest.local>
2021-06-24 13:49:32 +0200

    added new hookform recipe adder for applying context later on

--- src/Pages/Add-recipe-page/Step-2.js
+++ src/Pages/Add-recipe-page/Step-2.js
@@ -1,7 +1,9 @@
-import AddDirections from "../../components/AddDirections/AddDirections";
-import Button from "../../components/Button/Button";
+import React, {useState} from "react";
+import { useForm } from "react-hook-form";
+import {BsTrashFill} from "react-icons/bs";
 import {useHistory} from "react-router-dom";
-import AddIngredients from "../../components/AddIngredients/AddIngredients";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
 function AddRecipeStep2() {
     const history= useHistory();
@@ -13,34 +15,115 @@ function AddRecipeStep2() {
         history.push("/add-recipe/step-1");
     }
 
+    // ADD INGREDIENTS BUILD
+
+    const [indexes, setIndexes] = useState([0]);
+    const [counter, setCounter] = useState(1);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    const addIngredient = () => {
+        setIndexes((prevIndexes) => [...prevIndexes, counter]);
+        setCounter((prevCounter) => prevCounter + 1);
+    };
+
+    const removeIngredient = (index) => () => {
+        setIndexes((prevIndexes) => [
+            ...prevIndexes.filter((item) => item !== index)
+        ]);
+        setCounter((prevCounter) => prevCounter - 1);
+    };
+
+    const clearIngredients = () => {
+        setIndexes([]);
+    };
+    console.log(indexes);
     return (
         <>
             <div className="page-wrapper">
                 <div className="page-wrapper-inner">
                     <h1>Add a new recipe</h1>
                     <div className="add-ingredient-wrapper">
-                        <AddIngredients/>
+                        <form onSubmit={handleSubmit(onSubmit)} className="add-ingredient-form">
+                            <h2>Add an ingredient</h2>
+                            {indexes.map((index) => {
+                                const fieldName = `ingredient-${index}`;
+                                return (
+                                    <div name={fieldName} key={fieldName}>
+                                        <div className="ingredient-input-holder">
+                                            <Input
+                                                name={`${fieldName}`}
+                                                labelId="testId"
+                                                register={register}
+                                                placeholder="test"
+                                                type="text"
+                                                errors={errors}
+                                            />
+                                            <Button
+                                                type="button"
+                                                buttonTitle={<BsTrashFill className="eraserIcon"/>}
+                                                classNameButton="btn remove"
+                                                onClickEvent={removeIngredient(index)}
+                                            />
+                                        </div>
+
+                                    </div>
+                                )
+                            })}
+                            <div className="list-control-wrapper">
+                                <Button
+                                    type="button"
+                                    buttonTitle="Add ingredient"
+                                    onClickEvent={addIngredient}
+                                    classNameButton="btn"
+                                />
+                                <Button
+                                    type="button"
+                                    buttonTitle="Clear ingredient list"
+                                    onClickEvent={clearIngredients}
+                                    classNameButton="btn clear"
+                                />
+                            </div>
+                            <div className="button-wrapper">
+                                <Button
+                                    classNameButton="btn cancel"
+                                    buttonTitle="Previous step"
+                                    type="submit"
+                                    onClickEvent={previousStep}
+                                />
+                                <Button
+                                    buttonTitle="Save and continue"
+                                    type="submit"
+                                    classNameButton="btn save-and-continue"
+                                />
+                            </div>
+                        </form>
+
                     </div>
                 </div>
             </div>
-            <div className="page-wrapper blue">
-                <div className="page-wrapper-inner">
-                    <div className="button-wrapper">
-                        <Button
-                            classNameButton="btn cancel"
-                            buttonTitle="Previous step"
-                            type="submit"
-                            onClickEvent={previousStep}
-                        />
-                        <Button
-                            classNameButton="btn"
-                            buttonTitle="Save and continue"
-                            type="submit"
-                            onClickEvent={nextStep}
-                        />
-                    </div>
-                </div>
-            </div>
+
+            {/*<div className="page-wrapper blue">*/}
+            {/*    <div className="page-wrapper-inner">*/}
+            {/*        <div className="button-wrapper">*/}
+            {/*            <Button*/}
+            {/*                classNameButton="btn cancel"*/}
+            {/*                buttonTitle="Previous step"*/}
+            {/*                type="submit"*/}
+            {/*                onClickEvent={previousStep}*/}
+            {/*            />*/}
+            {/*            <Button*/}
+            {/*                classNameButton="btn"*/}
+            {/*                buttonTitle="Save and continue"*/}
+            {/*                type="submit"*/}
+            {/*                onClickEvent={nextStep}*/}
+            {/*            />*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </>
     )
 }
--- src/Pages/Style-page/Style-page.js
+++ src/Pages/Style-page/Style-page.js
@@ -1,12 +1,38 @@
 import './Style-page.css'
 import { HiSearch } from "react-icons/hi";
+import {useForm} from "react-hook-form";
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
 
 function StylePage() {
+
+    // const { register, handleSubmit, formState: { errors } } = useForm();
+    //
+    // const onSubmit = (data) => {
+    //     console.log(data);
+    // };
 
     return (
         <div className="page-wrapper">
             <div className="page-wrapper-inner">
                 <h1>Deze pagina is om nieuwe elementen te bouwen</h1>
+
+                {/*<form onSubmit={handleSubmit(onSubmit)}>*/}
+                {/*    <Input*/}
+                {/*        name="ingredient"*/}
+                {/*        type="text"*/}
+                {/*        labelId="ingredientId"*/}
+                {/*        register={register}*/}
+                {/*        errors={errors}*/}
+                {/*        required={true}*/}
+                {/*        requiredError="Required."*/}
+                {/*    />*/}
+                {/*    <Button*/}
+                {/*        buttonTitle="Send"*/}
+                {/*        classNameButton="btn"*/}
+                {/*        type="submit"*/}
+                {/*    />*/}
+                {/*</form>*/}
 
             </div>
         </div>
