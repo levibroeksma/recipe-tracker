dbee24d2c61e48b66989d39f888ece2a5df7f3cc
levibroeksma <levi@broeksma.nl>
2021-07-22 16:58:15 +0200

    cleaned up unused styling, tried adding sort function, WIP

--- src/Pages/Recipe-detail-page/RecipeDetailPage.js
+++ src/Pages/Recipe-detail-page/RecipeDetailPage.js
@@ -1,19 +1,28 @@
 import { useHistory, useParams} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
 import axios from "axios";
-import {useEffect, useState} from "react";
 import "./RecipeDetailPage.css"
 import LeaveComment from "../../components/Leave-comment/Leave-comment";
-import AverageRating from "../../components/AverageRating/AverageRating";
-import Comment from "../../components/Comment/Comment";
+import Button from "../../components/Button/Button";
+import {FaTrash} from "react-icons/fa";
+import {faStar} from "@fortawesome/free-solid-svg-icons";
+import {authContext} from "../../contexts/AuthContext";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 export default function RecipeDetailPage() {
     const [currentRecipe, setCurrentRecipe] = useState([]);
     const [blobRecipeImage, setBlobRecipeImage] = useState();
     const [allIngredients, setAllIngredients] = useState([]);
     const [allDirections, setAllDirections] = useState([]);
+    const [allReviews, setAllReviews] = useState([]);
+    const [hasReviewed, setHasReviewed] = useState(false);
+    const {user, isTokenValid} = useContext(authContext);
+    const star = <FontAwesomeIcon icon={faStar}/>
 
     const { id } = useParams();
     const history = useHistory();
+
 
     async function fetchRecipe() {
         try {
@@ -21,9 +30,17 @@ export default function RecipeDetailPage() {
             setCurrentRecipe(response.data)
             setAllIngredients(response.data.ingredients)
             setAllDirections(response.data.directions)
-        } catch (error) {}
+            setAllReviews(response.data.reviews.sort((a,b) => {
+                if (a.id > b.id) {return -1}
+                if (a.id < b.id) {return 1}
+                return 0;
+            }))
+        } catch (error) {
+            console.error('Something went wrong', error)
+        }
     }
 
+    console.log(currentRecipe)
     async function fetchRecipeImage() {
         try {
             const result = await axios.get(`http://localhost:8080/api/recipes/${id}/fileName`, {
@@ -93,7 +110,7 @@ export default function RecipeDetailPage() {
                             <div className="recipe-nav-holder">
                                 <button
                                     className="btn"
-                                        onClick={()=>{history.push("/recipes")}}
+                                    onClick={()=>{history.push("/recipes")}}
                                 >
                                     Overview
                                 </button>
@@ -102,34 +119,91 @@ export default function RecipeDetailPage() {
                     </section>
                 </div>
             </div>
-            <div className="page-wrapper blue">
-                <div className="page-wrapper-inner">
-                    <div className="recipe-text-holder">
-                    <LeaveComment/>
-                    <AverageRating/>
+            {isTokenValid() ? (
+                <>
+                    <div className="page-wrapper blue">
+                        <div className="page-wrapper-inner">
+                            <div className="recipe-text-holder">
+                                <LeaveComment
+                                    recipeId={id}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-
+                </>
+            )
+                :
+            (<></>)}
             <div className="page-wrapper">
                 <div className="page-wrapper-inner">
                     <div className="comment-wrapper">
-                        <Comment
-                            comment="text is coming here"
-                            username="Levi Broeksma"
-                        />
-                        <Comment
-                            comment="text is coming here"
-                            username="Levi Broeksma"
-                        />
-                        <Comment
-                            comment="text is coming here"
-                            username="Levi Broeksma"
-                        />
-                        <Comment
-                            comment="text is coming here"
-                            username="Levi Broeksma"
-                        />
+
+                        {allReviews && allReviews.map(review => {
+                            return <section className="comment-holder" key={"key" + review.id}>
+                                {user && user.authority === "ADMIN" && isTokenValid() &&
+                                (
+                                    <div className="delete-button">
+                                        <Button
+                                            type="submit"
+                                            buttonTitle={<FaTrash/>}
+                                            classNameButton="btn delete-comment"
+                                        />
+                                    </div>
+                                )
+                                }
+                                <div className="text-holder">
+                                    <p>{review.review}</p>
+                                </div>
+                                <div className="bottom-wrap">
+                                    <div className="account-comment-holder">
+                                        <div className="account-comment-image">
+                                            {review.userCountry ?
+                                                (
+                                                    <img
+                                                        src={require(`../../assets/flags/${review.userCountry}.png`).default}
+                                                        alt={review.userCountry}
+                                                    />
+                                                ) :
+                                                ("")
+                                            }
+
+                                        </div>
+                                        <div className="account-comment-text">
+                                            <h3>{review.username}</h3>
+                                        </div>
+                                    </div>
+                                    <div className="rating">
+                                        <div className="star-holder">
+                                            {review.stars === 1 &&
+                                            <div className="star-holder-one">
+                                                {star}{star}{star}{star}{star}
+                                            </div>
+                                            }
+                                            {review.stars === 2 &&
+                                            <div className="star-holder-two">
+                                                {star}{star}{star}{star}{star}
+                                            </div>
+                                            }
+                                            {review.stars === 3 &&
+                                            <div className="star-holder-three">
+                                                {star}{star}{star}{star}{star}
+                                            </div>
+                                            }
+                                            {review.stars === 4 &&
+                                            <div className="star-holder-four">
+                                                {star}{star}{star}{star}{star}
+                                            </div>
+                                            }
+                                            {review.stars === 5 &&
+                                            <div className="star-holder-five">
+                                                {star}{star}{star}{star}{star}
+                                            </div>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        })}
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AuthContextProvider from "./contexts/AuthContext";
-// import AddRecipeProvider from "./contexts/AddRecipeProvider";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -20,7 +19,4 @@ ReactDOM.render(
     document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
